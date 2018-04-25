9967c97adeaa670d5572612f8ecbd95b3b8c305a
V1shvesh <V1shvesh@users.noreply.github.com>
2018-04-25 19:29:22 +0530

    Add Fullpage-React

---
+++ App/app.jsx
@@ -0,0 +1,9 @@
+import { Fullpage, Slide, HorizontalSlider } from 'fullpage-react';
+
+const fullPageOptions = {
+    scrollsensitivity = 7,
+    touchSensitivity = 8,
+    scrollSpeed = 500,
+    hideScrollbars = true,
+    enableArrowKeys = true
+};
